c478888376231baeb2f9e01925c7094bbf0704d6
Braden <108199884+bradenr402@users.noreply.github.com>
2023-08-26 12:58:03 -0400

    Add grid axes below and to right of board

--- lib/board.rb
+++ lib/board.rb
@@ -1,12 +1,12 @@
-require_relative 'pieces/piece'
-require_relative 'pieces/piece_icons'
-require_relative 'pieces/pawn'
-require_relative 'pieces/rook'
-require_relative 'pieces/knight'
-require_relative 'pieces/bishop'
-require_relative 'pieces/king'
-require_relative 'pieces/queen'
-require_relative 'utilities/color'
+# require_relative 'pieces/piece'
+# require_relative 'pieces/piece_icons'
+# require_relative 'pieces/pawn'
+# require_relative 'pieces/rook'
+# require_relative 'pieces/knight'
+# require_relative 'pieces/bishop'
+# require_relative 'pieces/king'
+# require_relative 'pieces/queen'
+# require_relative 'utilities/color'
 
 class Board
   LETTERS = ('a'..'h').to_a
@@ -20,10 +20,10 @@ class Board
   end
 
   def display
-    puts "\n  " + " ".underline + LETTERS.join(' ').underline + " ".underline
+    puts "\n    " + LETTERS.join(' ') + "\n\n"
     i = 1
     @board.each do |row|
-      print i.to_s + '|'
+      print i.to_s + '  '
       row.each do |piece|
         if piece.is_a?(Piece)
           print piece.to_s
@@ -31,34 +31,34 @@ class Board
           print '  '
         end
       end
-      print "\n"
+      puts '   ' + i.to_s
       i += 1
     end
-    puts "\n"
+    puts "\n    " + LETTERS.join(' ') + "\n\n"
   end
 
   def setup
     for i in (0..7)
-      @board[i][1] = Pawn.new(:white)
-      @board[i][6] = Pawn.new(:black)
+      @board[6][i] = Pawn.new(:white, [6, i])
+      @board[1][i] = Pawn.new(:black, [1, i])
     end
 
-    @board[0][0] = Rook.new(:white)
-    @board[7][0] = Rook.new(:white)
-    @board[1][0] = Knight.new(:white)
-    @board[6][0] = Knight.new(:white)
-    @board[2][0] = Bishop.new(:white)
-    @board[5][0] = Bishop.new(:white)
-    @board[3][0] = King.new(:white)
-    @board[4][0] = Queen.new(:white)
+    @board[0][0] = Rook.new(:black, [0, 0])
+    @board[0][7] = Rook.new(:black, [0, 7])
+    @board[0][1] = Knight.new(:black, [0, 1])
+    @board[0][6] = Knight.new(:black, [0, 6])
+    @board[0][2] = Bishop.new(:black, [0, 2])
+    @board[0][5] = Bishop.new(:black, [0, 5])
+    @board[0][4] = King.new(:black, [0, 4])
+    @board[0][3] = Queen.new(:black, [0, 3])
 
-    @board[0][7] = Rook.new(:black)
-    @board[7][7] = Rook.new(:black)
-    @board[1][7] = Knight.new(:black)
-    @board[6][7] = Knight.new(:black)
-    @board[2][7] = Bishop.new(:black)
-    @board[5][7] = Bishop.new(:black)
-    @board[3][7] = King.new(:black)
-    @board[4][7] = Queen.new(:black)
+    @board[7][0] = Rook.new(:white, [7, 0])
+    @board[7][7] = Rook.new(:white, [7, 7])
+    @board[7][1] = Knight.new(:white, [7, 1])
+    @board[7][6] = Knight.new(:white, [7, 6])
+    @board[7][2] = Bishop.new(:white, [7, 2])
+    @board[7][5] = Bishop.new(:white, [7, 5])
+    @board[7][4] = King.new(:white, [7, 4])
+    @board[7][3] = Queen.new(:white, [7, 3])
   end
 end
