b7dbf0315048b270eb5dbf32fde2df360dac5c72
Braden <108199884+bradenr402@users.noreply.github.com>
2023-08-26 12:57:28 -0400

    Add position attribute to Piece class

--- lib/pieces/piece.rb
+++ lib/pieces/piece.rb
@@ -2,12 +2,15 @@ require_relative 'piece_icons'
 
 class Piece
   include PieceIcons
-  
-  def initialize(color)
+
+  def initialize(color, position)
     @color = color
+    @position = position
   end
 
   def legal_moves(board, current_position)
     # implement piece-specific movement logic
+      # return array of legal_moves
+    # ? better to include this in each child of Piece instead of here?
   end
 end
