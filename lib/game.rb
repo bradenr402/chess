94f2f00df7572a4bad981c8de5bcffc49f906ff9
Braden <108199884+bradenr402@users.noreply.github.com>
2023-08-26 14:04:19 -0400

    Write #prompt_move and #valid_move? methods

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,9 @@
+# require_relative 'board'
+# require_relative 'pieces/piece'
+
 class Game
   def initialize
-    @board = Chessboard.new
+    @board = Board.new
     @current_player = :white
   end
 
@@ -9,10 +12,15 @@ class Game
       @board.display
       move = prompt_move
       break if move == 'quit'
-      if valid_move?(move)
+
+      if move == 'save'
+        # save game
+        break
+      end
+
+      if legal_move?(move) && valid_move?(move)
         make_move(move)
         switch_player
-        # break
       else
         puts "Invalid move. Try again."
       end
@@ -20,14 +28,27 @@ class Game
   end
 
   def prompt_move
-    # prompt @current_player for a move
+    print "#{@current_player.color.capitalize}, your move: "
+    gets.chomp.downcase.split(' ') # returns move in format: ['xy', 'xy'], e.g. ['a1', 'b3']
   end
 
   def valid_move?(move)
-    # validate the move based on the current state of the board
+    return false unless move.size == 2
+    move.each do |coord|
+      return false unless coord.size == 2 && ('a'..'h').include?(coord.split('')[0]) && ('1'..'8').include?(coord.split('')[1])
+    end
+    true
   end
 
-  def make_move(move)
+  def legal_move?(move)
+    # validate that the move is legal based on the current state of the board
+      # i.e., the move is included in legal_moves (found in each Piece class)
+    # legal_moves = [Piece].legal_moves
+    # legal_moves.include?(move)
+    # * make sure the formats match
+  end
+
+  def move_piece(move)
     #update the board based on the move
   end
 
@@ -35,9 +56,18 @@ class Game
     @current_player = (@current_player == :white) ? :black : :white
   end
 
-  def check
+  def check?
     # detect if player's king is in check (probably at start of turn)
       # if true, restrict player's moves to escape check
-    # detect checkmate by analyzing all possible moves and determing if the king has no legal moves left
+      # if false, allow any legal moves
+  end
+
+  def checkmate?
+    # detect checkmate by analyzing all possible moves
+    # and determing if the king has no legal moves left
+  end
+
+  def stalemate?
+    # detect stalemate
   end
 end
