3b01965de32eca3834c31034f265bebd1faa2040
Braden <108199884+bradenr402@users.noreply.github.com>
2023-08-28 17:58:59 -0400

    Add pawn forward move functionality

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -2,11 +2,30 @@ require_relative 'piece'
 require_relative 'piece_icons'
 
 class Pawn < Piece
+  attr_accessor :has_moved
+
+  def initialize(color)
+    super
+    @has_moved = false
+  end
+
   def to_s
     pawn_icon(@color)
   end
 
-  def legal_moves(board, current_position)
-    # implement pawn moving and capturing logic
+  def legal_moves(board, current_player, current_position)
+    all_moves = []
+    if current_player.color == :black
+      all_moves << [current_position[0], current_position[1] + 1]
+      all_moves << [current_position[0], current_position[1] + 2] unless @has_moved
+    else
+      all_moves << [current_position[0], current_position[1] - 1]
+      all_moves << [current_position[0], current_position[1] - 2] unless @has_moved
+    end
+
+    # add en passant movement
+
+    all_moves
+
   end
 end
