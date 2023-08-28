4b8849ae2272e56d0c35f05ad64da20e90e70dec
Braden <108199884+bradenr402@users.noreply.github.com>
2023-08-28 18:29:58 -0400

    Add knight move functionality

--- lib/pieces/knight.rb
+++ lib/pieces/knight.rb
@@ -2,13 +2,28 @@ require_relative 'piece'
 require_relative 'piece_icons'
 
 class Knight < Piece
+  MOVES = [[1, 2], [-1, -2], [-1, 2], [1, -2], [2, 1], [-2, -1], [-2, 1], [2, -1]]
+
   def to_s
     knight_icon(@color)
   end
 
-  def legal_moves(board, current_position)
-    # implement knight movement and capturing logic
+  def legal_moves(board, current_player, current_position)
+    all_moves = []
 
-    # refer to Knights Travails project
+    MOVES.each do |move|
+      if (current_position[0] + move[0]).between?(0, 7) && (current_position[1] + move[1]).between?(0, 7)
+        all_moves << [current_position[0] + move[0], current_position[1] + move[1]]
+      end
+    end
+
+    all_moves.each do |square|
+      current_square = board.board[square[0]][square[1]]
+      if current_square.is_a?(Piece) && current_square.color == current_player.color
+        all_moves.delete(square)
+      end
+    end
+
+    all_moves
   end
 end
