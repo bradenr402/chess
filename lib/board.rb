20000858b1337d4f8b3e0a7e7eaca1cef9cb9a0b
Braden <108199884+bradenr402@users.noreply.github.com>
2023-08-28 15:17:50 -0400

    Rewrite #setup and #display methods

--- lib/board.rb
+++ lib/board.rb
@@ -9,6 +9,8 @@
 # require_relative 'utilities/color'
 
 class Board
+  attr_accessor :board
+
   LETTERS = ('a'..'h').to_a
   NUMBERS = (1..8).to_a
 
@@ -21,47 +23,49 @@ class Board
   def display
     system("clear") # clears the terminal
     puts "\n   #{LETTERS.join('  ')}"
-    i = 1
+    i = 0
     bg_light = true
-    @board.each do |row|
-      print "#{(9 - i).to_s} "
-      row.each do |piece|
+    while i < 8
+      print "#{8 - i} "
+      j = 0
+      while j < 8
         if bg_light
-          print piece.to_s.bg_gray
+          print @board[j][i].to_s.bg_gray
         else
-          print piece.to_s.bg_black
+          print @board[j][i].to_s.bg_black
         end
+        j += 1
         bg_light = !bg_light
       end
       bg_light = !bg_light
-      puts " #{(9 - i).to_s}"
+      print " #{8 - i}\n"
       i += 1
     end
-    puts "   #{LETTERS.join('  ')}\n\n"
+    puts "   #{LETTERS.join('  ')}"
   end
 
   def setup
     for i in (0..7)
-      @board[6][i] = Pawn.new(:white, [6, i])
-      @board[1][i] = Pawn.new(:black, [1, i])
+      @board[i][6] = Pawn.new(:white, [i, 6])
+      @board[i][1] = Pawn.new(:black, [i, 1])
     end
 
-    @board[7][0] = Rook.new(:white, [7, 0])
+    @board[0][7] = Rook.new(:white, [0, 7])
     @board[7][7] = Rook.new(:white, [7, 7])
-    @board[7][1] = Knight.new(:white, [7, 1])
-    @board[7][6] = Knight.new(:white, [7, 6])
-    @board[7][2] = Bishop.new(:white, [7, 2])
-    @board[7][5] = Bishop.new(:white, [7, 5])
-    @board[7][4] = King.new(:white, [7, 4])
-    @board[7][3] = Queen.new(:white, [7, 3])
+    @board[1][7] = Knight.new(:white, [1, 7])
+    @board[6][7] = Knight.new(:white, [6, 7])
+    @board[2][7] = Bishop.new(:white, [2, 7])
+    @board[5][7] = Bishop.new(:white, [5, 7])
+    @board[4][7] = King.new(:white, [4, 7])
+    @board[3][7] = Queen.new(:white, [3, 7])
 
     @board[0][0] = Rook.new(:black, [0, 0])
-    @board[0][7] = Rook.new(:black, [0, 7])
-    @board[0][1] = Knight.new(:black, [0, 1])
-    @board[0][6] = Knight.new(:black, [0, 6])
-    @board[0][2] = Bishop.new(:black, [0, 2])
-    @board[0][5] = Bishop.new(:black, [0, 5])
-    @board[0][4] = King.new(:black, [0, 4])
-    @board[0][3] = Queen.new(:black, [0, 3])
+    @board[7][0] = Rook.new(:black, [7, 0])
+    @board[1][0] = Knight.new(:black, [1, 0])
+    @board[6][0] = Knight.new(:black, [6, 0])
+    @board[2][0] = Bishop.new(:black, [2, 0])
+    @board[5][0] = Bishop.new(:black, [5, 0])
+    @board[4][0] = King.new(:black, [4, 0])
+    @board[3][0] = Queen.new(:black, [3, 0])
   end
 end
