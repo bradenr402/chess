6e42f1143d44dbf39327ba25c6c804aca8ab7859
Braden <108199884+bradenr402@users.noreply.github.com>
2023-08-29 21:15:30 -0400

    Add pawn attack functionality

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -16,17 +16,39 @@ class Pawn < Piece
   def legal_moves(board, current_player, current_position)
     moves = []
 
+    # add forward movement
     if current_player.color == :black
-      moves << [current_position.first, current_position.last + 1]
-      moves << [current_position.first, current_position.last + 2] unless @has_moved
+      square_in_front = board[current_position.first][current_position.last + 1]
+      moves << [current_position.first, current_position.last + 1] unless square_in_front.is_a?(Piece)
+
+      two_squares_in_front = board[current_position.first][current_position.last + 2]
+      moves << [current_position.first, current_position.last + 2] unless @has_moved || two_squares_in_front.is_a?(Piece)
     else
-      moves << [current_position.first, current_position.last - 1]
-      moves << [current_position.first, current_position.last - 2] unless @has_moved
+      square_in_front = board[current_position.first][current_position.last - 1]
+      moves << [current_position.first, current_position.last - 1] unless square_in_front.is_a?(Piece)
+
+      two_squares_in_front = board[current_position.first][current_position.last - 2]
+      moves << [current_position.first, current_position.last - 2] unless @has_moved || two_squares_in_front.is_a?(Piece)
+    end
+
+    # add attack movement
+    if current_player.color == :black
+      diagonal_left = board[current_position.first - 1][current_position.last + 1]
+      moves << [current_position.first - 1, current_position.last + 1] if diagonal_left.color == :white
+
+      diagonal_right = board[current_position.first + 1][current_position.last + 1]
+      moves << [current_position.first + 1, current_position.last + 1] if diagonal_right.color == :white
+    else
+      diagonal_left = board[current_position.first - 1][current_position.last - 1]
+      moves << [current_position.first - 1, current_position.last - 1] if diagonal_left.color == :black
+
+      diagonal_right = board[current_position.first + 1][current_position.last - 1]
+      moves << [current_position.first + 1, current_position.last - 1] if diagonal_right.color == :black
     end
 
     # add en passant movement
 
-    moves
 
+    moves
   end
 end
