3d57d626fa085a9a518389774c703a4dbb934bca
Braden <108199884+bradenr402@users.noreply.github.com>
2023-08-29 20:41:03 -0400

    Add rook move functionality

--- lib/pieces/rook.rb
+++ lib/pieces/rook.rb
@@ -6,7 +6,43 @@ class Rook < Piece
     rook_icon(@color)
   end
 
-  def legal_moves(board, current_position)
-    # implement rook movement and capturing logic
+  def legal_moves(board, current_player, current_position)
+    moves = []
+
+    x = current_position.first
+    y = current_position.last
+
+    until x == 7
+      x += 1
+      current_piece = board[x][y]
+      moves << [x, y] unless current_piece.color == current_player.color
+      break if current_piece.is_a?(Piece)
+    end
+    x = current_position.first
+
+    until x == 0
+      x -= 1
+      current_piece = board[x][y]
+      moves << [x, y] unless current_piece.color == current_player.color
+      break if current_piece.is_a?(Piece)
+    end
+    x = current_position.first
+
+    until y == 7
+      y += 1
+      current_piece = board[x][y]
+      moves << [x, y] unless current_piece.color == current_player.color
+      break if current_piece.is_a?(Piece)
+    end
+    y = current_position.last
+
+    until y == 0
+      y -= 1
+      current_piece = board[x][y]
+      moves << [x, y] unless current_piece.color == current_player.color
+      break if current_piece.is_a?(Piece)
+    end
+
+    moves
   end
 end
