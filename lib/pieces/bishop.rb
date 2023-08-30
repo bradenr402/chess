2d1bfbae04cc4226e3162833c386bf1a401da0d8
Braden <108199884+bradenr402@users.noreply.github.com>
2023-08-29 20:47:59 -0400

    Add bishop move functionality

--- lib/pieces/bishop.rb
+++ lib/pieces/bishop.rb
@@ -6,7 +6,52 @@ class Bishop < Piece
     bishop_icon(@color)
   end
 
-  def legal_moves(board, current_position)
+  def legal_moves(board, current_player, current_position)
     # implement bishop movement and capturing logic
+
+    moves = []
+
+    x = current_position.first
+    y = current_position.last
+
+    until x == 7 || y == 7
+      x += 1
+      y += 1
+      current_piece = board[x][y]
+      moves << [x, y] unless current_piece.color == current_player.color
+      break if current_piece.is_a?(Piece)
+    end
+    x = current_position.first
+    y = current_position.last
+
+    until x == 7 || y == 0
+      x += 1
+      y -= 1
+      current_piece = board[x][y]
+      moves << [x, y] unless current_piece.color == current_player.color
+      break if current_piece.is_a?(Piece)
+    end
+    x = current_position.first
+    y = current_position.last
+
+    until x == 0 || y == 7
+      x -= 1
+      y += 1
+      current_piece = board[x][y]
+      moves << [x, y] unless current_piece.color == current_player.color
+      break if current_piece.is_a?(Piece)
+    end
+    x = current_position.first
+    y = current_position.last
+
+    until x == 0 || y == 0
+      x -= 1
+      y -= 1
+      current_piece = board[x][y]
+      moves << [x, y] unless current_piece.color == current_player.color
+      break if current_piece.is_a?(Piece)
+    end
+
+    moves
   end
 end
