03dff51ccbb1174aa4d7d7aff722d170dd1fb1b8
Braden <108199884+bradenr402@users.noreply.github.com>
2023-08-29 21:42:02 -0400

    Separate #get_forward_moves and #get_attacks methods from #legal_moves

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -17,38 +17,53 @@ class Pawn < Piece
     moves = []
 
     # add forward movement
-    if current_player.color == :black
-      square_in_front = board[current_position.first][current_position.last + 1]
-      moves << [current_position.first, current_position.last + 1] unless square_in_front.is_a?(Piece)
-
-      two_squares_in_front = board[current_position.first][current_position.last + 2]
-      moves << [current_position.first, current_position.last + 2] unless @has_moved || two_squares_in_front.is_a?(Piece)
-    else
-      square_in_front = board[current_position.first][current_position.last - 1]
-      moves << [current_position.first, current_position.last - 1] unless square_in_front.is_a?(Piece)
-
-      two_squares_in_front = board[current_position.first][current_position.last - 2]
-      moves << [current_position.first, current_position.last - 2] unless @has_moved || two_squares_in_front.is_a?(Piece)
-    end
+    forward_moves = get_forward_moves(board, current_player, current_position, moves)
 
     # add attack movement
-    if current_player.color == :black
-      diagonal_left = board[current_position.first - 1][current_position.last + 1]
-      moves << [current_position.first - 1, current_position.last + 1] if diagonal_left.color == :white
-
-      diagonal_right = board[current_position.first + 1][current_position.last + 1]
-      moves << [current_position.first + 1, current_position.last + 1] if diagonal_right.color == :white
-    else
-      diagonal_left = board[current_position.first - 1][current_position.last - 1]
-      moves << [current_position.first - 1, current_position.last - 1] if diagonal_left.color == :black
-
-      diagonal_right = board[current_position.first + 1][current_position.last - 1]
-      moves << [current_position.first + 1, current_position.last - 1] if diagonal_right.color == :black
-    end
+    attacks = get_attacks(board, current_player, current_position, moves)
 
     # add en passant movement
 
+    forward_moves + attacks
+  end
 
-    moves
+  def get_forward_moves(board, current_player, current_position, moves)
+    forward_moves = moves
+
+    if current_player.color == :black
+      square_in_front = board[current_position.first][current_position.last + 1]
+      forward_moves << [current_position.first, current_position.last + 1] unless square_in_front.is_a?(Piece)
+
+      two_squares_in_front = board[current_position.first][current_position.last + 2]
+      forward_moves << [current_position.first, current_position.last + 2] unless @has_moved || two_squares_in_front.is_a?(Piece)
+    else
+      square_in_front = board[current_position.first][current_position.last - 1]
+      forward_moves << [current_position.first, current_position.last - 1] unless square_in_front.is_a?(Piece)
+
+      two_squares_in_front = board[current_position.first][current_position.last - 2]
+      forward_moves << [current_position.first, current_position.last - 2] unless @has_moved || two_squares_in_front.is_a?(Piece)
+    end
+
+    forward_moves
+  end
+
+  def get_attacks(board, current_player, current_position, moves)
+    attacks = moves
+
+    if current_player.color == :black
+      diagonal_left = board[current_position.first - 1][current_position.last + 1]
+      attacks << [current_position.first - 1, current_position.last + 1] if diagonal_left.color == :white
+
+      diagonal_right = board[current_position.first + 1][current_position.last + 1]
+      attacks << [current_position.first + 1, current_position.last + 1] if diagonal_right.color == :white
+    else
+      diagonal_left = board[current_position.first - 1][current_position.last - 1]
+      attacks << [current_position.first - 1, current_position.last - 1] if diagonal_left.color == :black
+
+      diagonal_right = board[current_position.first + 1][current_position.last - 1]
+      attacks << [current_position.first + 1, current_position.last - 1] if diagonal_right.color == :black
+    end
+
+    attacks
   end
 end
