b11bb22b57ceb8a5b1b5130214e8f8da46b1e8de
Braden <108199884+bradenr402@users.noreply.github.com>
2023-08-29 20:54:46 -0400

    Add queen move functionality

--- lib/pieces/queen.rb
+++ lib/pieces/queen.rb
@@ -6,7 +6,17 @@ class Queen < Piece
     queen_icon(@color)
   end
 
-  def legal_moves(board, current_position)
+  def legal_moves(board, current_player, current_position)
     # implement queen movement and capturing logic
+    # * shortcut: queen's moves = rook's moves + bishop's moves
+
+    rook = Rook.new(current_player.color)
+    bishop = Bishop.new(current_player.color)
+
+    moves = []
+    rook_moves = rook.legal_moves(board, current_player, current_position)
+    bishop_moves = bishop.legal_moves(board, current_player, current_position)
+
+    rook_moves + bishop_moves
   end
 end
