12de71eb3f1889c0324c9cf9d339aa8eb66b83d1
Braden <108199884+bradenr402@users.noreply.github.com>
2023-09-01 18:25:38 -0400

    Simplify getting rook_moves & bishop_moves for queen

--- lib/pieces/queen.rb
+++ lib/pieces/queen.rb
@@ -7,15 +7,8 @@ class Queen < Piece
   end
 
   def legal_moves(board, current_player, current_position)
-    # implement queen movement and capturing logic
-    # * shortcut: queen's moves = rook's moves + bishop's moves
-
-    rook = Rook.new(current_player.color)
-    bishop = Bishop.new(current_player.color)
-
-    moves = []
-    rook_moves = rook.legal_moves(board, current_player, current_position)
-    bishop_moves = bishop.legal_moves(board, current_player, current_position)
+    rook_moves = Rook.new(current_player.color).legal_moves(board, current_player, current_position)
+    bishop_moves = Bishop.new(current_player.color).legal_moves(board, current_player, current_position)
 
     rook_moves + bishop_moves
   end
