350bc3924aba31881243b33fca7a05af4bc3ec4f
Braden <108199884+bradenr402@users.noreply.github.com>
2023-09-01 18:02:37 -0400

    Modularize legal_moves into separate methods for left, right, up, and down movement

--- lib/pieces/rook.rb
+++ lib/pieces/rook.rb
@@ -9,40 +9,71 @@ class Rook < Piece
   def legal_moves(board, current_player, current_position)
     moves = []
 
+    moves_right = get_moves_right(board, current_player, current_position)
+    moves_left = get_moves_left(board, current_player, current_position)
+    moves_down = get_moves_down(board, current_player, current_position)
+    moves_up = get_moves_up(board, current_player, current_position)
+
+    moves_right + moves_left + moves_up + moves_down
+  end
+
+  def get_moves_left(board, current_player, current_position)
     x = current_position.first
     y = current_position.last
-
-    until x == 7
-      x += 1
-      current_piece = board[x][y]
-      moves << [x, y] unless current_piece.color == current_player.color
-      break if current_piece.is_a?(Piece)
-    end
-    x = current_position.first
+    moves_left = []
 
     until x == 0
       x -= 1
-      current_piece = board[x][y]
-      moves << [x, y] unless current_piece.color == current_player.color
-      break if current_piece.is_a?(Piece)
+      current_square = board[x][y]
+      moves_left << [x, y] unless current_square.color == current_player.color
+      break if current_square.is_a?(Piece)
     end
-    x = current_position.first
 
-    until y == 7
-      y += 1
-      current_piece = board[x][y]
-      moves << [x, y] unless current_piece.color == current_player.color
-      break if current_piece.is_a?(Piece)
-    end
+    moves_left
+  end
+
+  def get_moves_right(board, current_player, current_position)
+    x = current_position.first
     y = current_position.last
+    moves_right = []
+
+    until x == 7
+      x += 1
+      current_square = board[x][y]
+      moves_right << [x, y] unless current_square.color == current_player.color
+      break if current_square.is_a?(Piece)
+    end
+
+    moves_right
+  end
+
+  def get_moves_up(board, current_player, current_position)
+    x = current_position.first
+    y = current_position.last
+    moves_up = []
 
     until y == 0
       y -= 1
-      current_piece = board[x][y]
-      moves << [x, y] unless current_piece.color == current_player.color
-      break if current_piece.is_a?(Piece)
+      current_square = board[x][y]
+      moves_up << [x, y] unless current_square.color == current_player.color
+      break if current_square.is_a?(Piece)
     end
 
-    moves
+    moves_up
+  end
+
+  def get_moves_down(board, current_player, current_position)
+    x = current_position.first
+    y = current_position.last
+    moves_down = []
+
+    until y == 7
+      y += 1
+      current_square = board[x][y]
+      moves_down << [x, y] unless current_square.color == current_player.color
+      break if current_square.is_a?(Piece)
+    end
+
+    moves_down
   end
 end
