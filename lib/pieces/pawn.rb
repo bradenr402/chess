f44ba2ca840edee43aa18d3120bfd3e412838cc3
Braden <108199884+bradenr402@users.noreply.github.com>
2023-09-04 16:25:17 -0400

    Fix spacing

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -32,13 +32,13 @@ class Pawn < Piece
       forward_moves << [x, y + 1] unless square_in_front.is_a?(Piece)
 
       two_squares_in_front = board[x][y + 2]
-      forward_moves << [x, y + 2] unless @has_moved ||square_in_front.is_a?(Piece) || two_squares_in_front.is_a?(Piece)
+      forward_moves << [x, y + 2] unless @has_moved || square_in_front.is_a?(Piece) || two_squares_in_front.is_a?(Piece)
     else
       square_in_front = board[x][y - 1]
       forward_moves << [x, y - 1] unless square_in_front.is_a?(Piece)
 
       two_squares_in_front = board[x][y - 2]
-      forward_moves << [x, y - 2] unless @has_moved ||square_in_front.is_a?(Piece)  || two_squares_in_front.is_a?(Piece)
+      forward_moves << [x, y - 2] unless @has_moved || square_in_front.is_a?(Piece) || two_squares_in_front.is_a?(Piece)
     end
 
     forward_moves
