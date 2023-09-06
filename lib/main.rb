976064b427745a0adba65ecb034377764a71c7fb
Braden <108199884+bradenr402@users.noreply.github.com>
2023-09-05 21:40:16 -0400

    Use get_char for deciding whether to play again

--- lib/main.rb
+++ lib/main.rb
@@ -13,6 +13,8 @@ require_relative 'pieces/king'
 require_relative 'pieces/queen'
 require_relative 'empty_square'
 require_relative 'utilities/get_char'
+include GetChar
+require "pry-byebug"
 
 def start_game
   loop do
@@ -26,12 +28,10 @@ def play_again?
     puts 'Do you want to play again?'
     puts "#{'[1]'.green} Play another game"
     puts "#{'[2]'.red} Exit"
-    input = gets.chomp
+    input = get_char
     return true if input == '1'
     return false if input == '2'
   end
 end
 
-include GetChar
-require "pry-byebug"
 start_game
