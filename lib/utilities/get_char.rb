134138a137d1c1d5417352a873da0919641a51d5
Braden <108199884+bradenr402@users.noreply.github.com>
2023-09-06 16:55:46 -0400

    Remove module around #get_char method

--- lib/utilities/get_char.rb
+++ lib/utilities/get_char.rb
@@ -1,8 +1,6 @@
-module GetChar
-  def get_char
-    input = STDIN.getch
-    control_c_code = "\u0003"
-    exit(1) if input == control_c_code
-    input
-  end
+def get_char
+  input = STDIN.getch
+  control_c_code = "\u0003"
+  exit(1) if input == control_c_code
+  input
 end
