d24442f49f6b08fe19b37d43b4e6a65f90004e68
Braden <108199884+bradenr402@users.noreply.github.com>
2023-09-06 16:56:21 -0400

    Rearrange require_relative lines for better readability

--- lib/main.rb
+++ lib/main.rb
@@ -1,8 +1,8 @@
 require_relative 'game'
 require_relative 'player'
 require_relative 'board'
-require_relative 'utilities/color'
-require_relative 'utilities/save_load'
+require_relative 'empty_square'
+
 require_relative 'pieces/piece'
 require_relative 'pieces/piece_icons'
 require_relative 'pieces/pawn'
@@ -11,19 +11,20 @@ require_relative 'pieces/knight'
 require_relative 'pieces/bishop'
 require_relative 'pieces/king'
 require_relative 'pieces/queen'
-require_relative 'empty_square'
+
+require_relative 'utilities/color'
+require_relative 'utilities/save_load'
 require_relative 'utilities/get_char'
 require_relative 'utilities/game_rules'
 require_relative 'utilities/castling'
 require_relative 'utilities/promotion'
 require_relative 'utilities/en_passant'
 require_relative 'utilities/referee'
-include GetChar
 require 'pry-byebug'
 
 def start_game
   loop do
-    game = Game.new.play_game
+    @game = Game.new.play_game
     break unless play_again?
   end
 end
